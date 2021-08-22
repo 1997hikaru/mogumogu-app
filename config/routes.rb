397497097da80c2cb35b1df7a702f02c5ab62c18
1997hikaru <hikarutakata0224@gmail.com>
2021-08-22 22:08:05 +0900

    model/controllerの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :buyers,  controllers: {
+    sessions:       'buyers/sessions',
+    passwords:      'buyers/passwords',
+    registrations:  'buyers/registrations'
+  }
+  devise_for :sellers, controllers: {
+    sessions:       'sellers/sessions',
+    passwords:      'sellers/passwords',
+    registrations:  'sellers/registrations'
+  }
   root to: "items#index"
 end
